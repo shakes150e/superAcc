8163de32ce98603393a5959ad424ae9ca2d6031d
Asanda <205049990@mycput.ac.za>
2021-04-20 17:19:06 +0200

    created a new class

---
+++ src/test/java/ac/za/cput/superAcc/dvdTest.java
@@ -0,0 +1,16 @@
+package ac.za.cput.superAcc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class dvdTest {
+    @Test
+    void testEquality(){
+        assertEquals(1,1);
+
+    }
+
+
+}
